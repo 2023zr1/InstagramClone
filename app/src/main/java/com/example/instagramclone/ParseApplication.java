ec733e48ed55e3360a2a6faa90a5092b5709accc
2023zr1 <shreya.m.2023@gmail.com>
2021-03-13 01:02:38 -0800

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,24 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("e3Q0spXEdds4mCS1MrimmN3rbGcJ8wjgM8vyyLFH")
+                .clientKey("arwJOGeJ23Zzf0S6UbTUPl1IysFZ5G1CYcdY2Xcb")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
---
+++ app/src/main/java/com/example/instagramclone/TimelineActivity.java
@@ -0,0 +1,15 @@
+package com.example.instagramclone;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseUser;
+
+public class TimelineActivity extends AppCompatActivity {
+
+
+
+}
